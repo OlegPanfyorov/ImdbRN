bc558fde1f23d41d038958cfa196ac032f82cb03
Oleg Panfyorov <olegpanfyorov@gmail.com>
2019-02-15 16:28:10 +0200

    Android worked version

--- android/app/src/main/java/com/imdbproject/MainApplication.java
+++ android/app/src/main/java/com/imdbproject/MainApplication.java
@@ -34,8 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
             new SvgPackage(),
             new SplashScreenReactPackage(),
             new RNGestureHandlerPackage(),
-            new RNFetchBlobPackage(),
-            new RNCWebViewPackage()
+            new RNFetchBlobPackage()
             );
         }
 
